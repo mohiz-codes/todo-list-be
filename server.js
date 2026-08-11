7adbe3796c404721d5eef6d3a33a2fe2b4a2f412
mohiz-codes <mohizkhan1111@gmail.com>
2026-08-11 15:21:03 +0500

    init BE

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    console.log("[+] running : http://localhost:3000");
+});
