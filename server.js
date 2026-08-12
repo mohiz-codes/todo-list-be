317a85fb561f884b5ba78fffda830bd900812911
mohiz-codes <mohizkhan1111@gmail.com>
2026-08-12 21:00:43 +0500

    CRUD Complete

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
+let todos = [];
+//READ
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+//CREATE
+app.post("/todos", (req, res) => {
+  let newTask = {
+    id: Date.now(),
+    text: req.body.text,
+    status: req.body.status,
+  };
+  todos.push(newTask);
+  console.log(todos);
+  res.json(newTask); //result variable is waiting for some value which it will use to spread the task array this sends that in json format
+});
+//UPDATE
+app.put("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const task = todos.find((todo) => todo.id === id);
 
-app.get("/", (req, res) => {
-    res.send("Hello");
+  if (req.body.status !== undefined) {
+    task.status = req.body.status;
+  }
+
+  if (req.body.text !== undefined) {
+    task.text = req.body.text;
+  }
+
+  res.json(task);
+});
+//DELETE
+app.delete("/todos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const task = todos.find((todo) => todo.id == id);
+  todos = todos = todos.filter((todo) => todo.id !== id);
+  res.json(todos);
+  console.log(todos);
 });
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-    console.log("[+] running : http://localhost:3000");
+  console.log("Server running on port 3000");
 });
